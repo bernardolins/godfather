80982e175b6ea0014a36a4b907fa2aa2c03094f3
Bernardo Lins <bernardolins28@gmail.com>
2015-11-22 12:25:36 -0200

    Add some data to yaml structure

--- task/types.go
+++ task/types.go
@@ -2,6 +2,14 @@ package task
 
 import ()
 
-type T struct {
-	Name string
+type Task struct {
+	Metadata Metadata          `yaml: "metadata"`
+	Env      map[string]string `yaml: "env,omitempty"`
+}
+
+type Metadata struct {
+	// Name is the name of the project
+	Name string `yaml: "name"`
+	// Selectors is aditional metadata to select groups of projects
+	Selectors map[string]string `yaml: "selectors"`
 }
