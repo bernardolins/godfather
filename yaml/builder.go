3cad333a46388128dce6261384de5c97bf52aa4b
Bernardo Lins <bernardolins28@gmail.com>
2015-11-22 14:29:41 -0200

    Calling task validation after build a task

--- yaml/builder.go
+++ yaml/builder.go
@@ -1,6 +1,7 @@
 package yaml
 
 import (
+	"fmt"
 	"github.com/bernardolins/godfather/task"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -13,12 +14,16 @@ func BuildTask(path string) task.Task {
 		panic(err)
 	}
 
-	var task task.Task
-	err = yaml.Unmarshal(f, &task)
+	var t task.Task
+	err = yaml.Unmarshal(f, &t)
+
+	if !task.Validate(t) {
+		fmt.Println("An error occurred when creating task at", path)
+	}
 
 	if err != nil {
 		panic(err)
 	}
 
-	return task
+	return t
 }
