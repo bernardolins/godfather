7ede98fe859c718ca842c1b54d56c79a706553e9
Bernardo Lins <bernardolins28@gmail.com>
2015-11-22 23:36:58 -0200

    Add tests to validation

---
+++ task/validate_test.go
@@ -0,0 +1,53 @@
+package task
+
+import (
+	"testing"
+)
+
+func TestIsValidName(t *testing.T) {
+	cases := []struct {
+		testCase string
+		expect   bool
+	}{
+		{"test", true},
+		{"test1", true},
+		{"test-1", true},
+		{"test_1", true},
+		{"Test", true},
+		{"test.1", false},
+		{"test%", false},
+		{"test/", false},
+	}
+
+	for _, c := range cases {
+		got := isValidName(c.testCase)
+		if c.expect != got {
+			t.Errorf("For filename %q, Expect %t, got %t", c.testCase, c.expect, got)
+		}
+	}
+
+}
+
+//func TestValidateMetadata(t *testing.T) {
+//	cases := []struct {
+//		testCase string
+//		expect   bool
+//	}{
+//		{"test", true},
+//		{"test1", true},
+//		{"test-1", true},
+//		{"test_1", true},
+//		{"Test", true},
+//		{"test.1", false},
+//		{"test%", false},
+//		{"test/", false},
+//	}
+//
+//	for _, c := range cases {
+//		got := isValidName(c.testCase)
+//		if c.expect != got {
+//			t.Errorf("For filename %q, Expect %t, got %t", c.testCase, c.expect, got)
+//		}
+//	}
+//
+//}
