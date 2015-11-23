2c29a86a36adf72f5a85f9f4f3690a2f6011b8ce
Bernardo Lins <bernardolins28@gmail.com>
2015-11-23 01:42:28 -0200

    Calling command exec as goroutine

--- yaml/builder.go
+++ yaml/builder.go
@@ -2,9 +2,11 @@ package yaml
 
 import (
 	"fmt"
+	"github.com/bernardolins/godfather/exec"
 	"github.com/bernardolins/godfather/spec"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"log"
 )
 
 func BuildTask(path string) spec.Task {
@@ -19,6 +21,9 @@ func BuildTask(path string) spec.Task {
 
 	if !spec.Validate(t) {
 		fmt.Println("An error occurred when creating task at", path)
+	} else {
+		log.Printf("Running task %s", t.Metadata.Name)
+		go exec.Exec(t)
 	}
 
 	if err != nil {
