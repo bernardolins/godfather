64fb39b21154f72ae4a502b38ae01fafd92150ab
Bernardo Lins <bernardolins28@gmail.com>
2015-11-23 00:05:55 -0200

    Create command field of yaml

--- spec/types.go
+++ spec/types.go
@@ -5,6 +5,7 @@ import ()
 type Task struct {
 	Metadata Metadata          `yaml: "metadata"`
 	Env      map[string]string `yaml: "env,omitempty"`
+	Command  Command           `yaml: "command"`
 }
 
 type Metadata struct {
@@ -12,4 +13,9 @@ type Metadata struct {
 	Name string `yaml: "name"`
 	// Selectors is aditional metadata to select groups of projects
 	Selectors map[string]string `yaml: "selectors,omitempty"`
+}
+
+type Command struct {
+	Exec      string   `yaml: "exec"`
+	Arguments []string `yaml: "arguments"`
 }
